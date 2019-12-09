ce19e74993f4f624822ad0fedd394421d48e4151
consuelo-sanna <consu.sanna@gmail.com>
2019-12-09 17:30:59 +0100

    Updates

---
+++ precache-manifest.6871ab4092632c047b7227d7a4d9efce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9139178cc9b977e48f9ef0a1b96bc71b",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "e57c4a0dd02ad3dfe43d",
+    "url": "/React-todo/static/js/2.adb4071c.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/React-todo/static/js/2.adb4071c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e9775ddf4526ed22bb7",
+    "url": "/React-todo/static/js/main.042d418d.chunk.js"
+  },
+  {
+    "revision": "e5ed9f00bb3f2829f0c9",
+    "url": "/React-todo/static/js/runtime-main.fc85c398.js"
+  }
+]);
