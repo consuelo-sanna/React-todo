85f3c7300d5c693c5cf75583894148b6503e589e
consuelo-sanna <consu.sanna@gmail.com>
2019-12-09 17:16:37 +0100

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+//rce tab
+
+/* Se crei un metodo custom, quindi diverso dai soliti devi fare il bind
+ * perchè sia visibile all'interno della tua classe.. anche se è li dentro
+ * Con l'arrow function non è necessario invece..
+*/ 
+
+export class TodoItem extends Component {
+
+    getStyle = () =>{
+        return{
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
+    
+    
+    render() {
+        const { id, title } = this.props.todo;
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input type='checkbox' onChange={this.props.markComplete.bind(
+                        this,id )}/>
+                     {title } 
+                     <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+                </p>
+            </div>
+        )
+    }
+}
+
+//validation to make sure the prop has the right type
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+}
+
+const btnStyle={
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius:'50%',
+    cursors:'pointer',
+    float:'right'
+
+}
+
+export default TodoItem
