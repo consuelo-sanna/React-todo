68be1899d887d4ac27b0082790052b9d595f8fc2
Consuelo Sanna <consu.sanna@gmail.com>
2019-12-15 17:15:51 +0100

    Update About.js
    s

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,6 +4,7 @@ export default function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
+        
             <p> This is the TodoList app</p><br/>
             <p> This app was entirely made with react framework. </p><br/>
             <p> The data are avaiable at the todoData page of the server</p><br/>
