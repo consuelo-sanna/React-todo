1e4ecad360733068519b19bb6e3650457bd384e7
consuelo-sanna <consu.sanna@gmail.com>
2019-12-15 18:07:15 +0100

    prova branch

---
+++ build/precache-manifest.0eb38079aa61da8c66965e2b5e8a4634.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89ebd66337c2305a45da411ab908cb7",
+    "url": "/consuelo-sanna/React-todo/index.html"
+  },
+  {
+    "revision": "e57c4a0dd02ad3dfe43d",
+    "url": "/consuelo-sanna/React-todo/static/js/2.adb4071c.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/consuelo-sanna/React-todo/static/js/2.adb4071c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dde91908ceb9e390e469",
+    "url": "/consuelo-sanna/React-todo/static/js/main.63b25f0a.chunk.js"
+  },
+  {
+    "revision": "cbf5e868868ec370d532",
+    "url": "/consuelo-sanna/React-todo/static/js/runtime-main.23f8503c.js"
+  }
+]);
